756ae44196cc310586118767b4273f11c831284d
Lucas <lvt655@gmail.com>
2026-09-01 16:35:40 -0400

    Use custom icons in shared game components

--- client/src/components/dashboard/widgets/LeaderboardWidget.tsx
+++ client/src/components/dashboard/widgets/LeaderboardWidget.tsx
@@ -1,3 +1,4 @@
+import { TradebattleIcon } from "@/components/tradebattle-icons";
 import { WidgetWrapper } from "./WidgetWrapper";
 import { Trophy } from "lucide-react";
 import { TournamentLeaderboard } from "@/components/tournaments/TournamentLeaderboard";
@@ -16,7 +17,7 @@ export function LeaderboardWidget({
   return (
     <WidgetWrapper
       title="Rankings"
-      icon={<Trophy className="w-4 h-4" />}
+      icon={<TradebattleIcon name="rankings" className="w-4 h-4" />}
       onRemove={onRemove}
     >
       <TournamentLeaderboard
