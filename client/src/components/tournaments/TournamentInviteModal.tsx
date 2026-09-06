d85aaf6c45a60f0bbeb3777eefb7b550fca1a5cb
Lucas <lvt655@gmail.com>
2026-09-05 22:19:34 -0400

    Use the simpler avatar for all invite states

--- client/src/components/tournaments/TournamentInviteModal.tsx
+++ client/src/components/tournaments/TournamentInviteModal.tsx
@@ -144,7 +144,7 @@ export default function TournamentInviteModal({
                         onCheckedChange={() => handleToggleFriend(friend.id)}
                       />
                       <Avatar className="h-10 w-10">
-                        <AvatarImage src={friend.profilePicture || "/assets/tradebattle-default-broker.png"} />
+                        <AvatarImage src={friend.profilePicture || "/assets/tradebattle-default-broker-v2.png"} />
                         <AvatarFallback style={{ background: '#0E2040', color: '#C9D1E2' }}>
                           {friend.username.charAt(0).toUpperCase()}
                         </AvatarFallback>
@@ -171,7 +171,7 @@ export default function TournamentInviteModal({
                       }}
                     >
                       <Avatar className="h-10 w-10">
-                        <AvatarImage src={friend.profilePicture} />
+                        <AvatarImage src={friend.profilePicture || "/assets/tradebattle-default-broker-v2.png"} />
                         <AvatarFallback style={{ background: '#0E2040', color: '#C9D1E2' }}>
                           {friend.username.charAt(0).toUpperCase()}
                         </AvatarFallback>
